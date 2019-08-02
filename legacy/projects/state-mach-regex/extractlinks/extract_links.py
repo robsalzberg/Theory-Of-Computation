92e5ab30412859baadb5c48a35bfeeaf84eaed76
robsalzberg <robsalzberg@gmail.com>
2019-08-02 00:22:05 -0400

    Refactor extract_links.py

--- legacy/projects/state-mach-regex/extractlinks/extract_links.py
+++ legacy/projects/state-mach-regex/extractlinks/extract_links.py
@@ -14,39 +14,42 @@ filename = sys.argv[1]
 
 # TODO Read HTML file
 
-filedata = ''
-with open(filename) as file:
-    filedata = file.read()
-file.close()
+f = open(filename, 'r')
+source = f.read()
 
 # TODO Set up regex
 
-regex =
-'https?:\/\/[^\"\ \<\>\{\}\|\\\^\~\[\]\']+?\.[^\"\ \<\>\{\}\|\\\^\~\[\]\']+'
+regex = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 # TODO Find links using regex, save in list called 'matches'
 
-matches = re.findall(regex, filedata)
+matches = re.findall(regex, source)
 
 # Check matches, print results
+
+print(matches)
+
 # TODO Read in links from answers.txt (hint...this is a CSV file)
 # save in list called 'answer_data'
 
-with open("answers.txt") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+answer_data = []
+
+with open('answers.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
     for row in csv_reader:
         answer_data = row
 
+print(answer_data)
+
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
-# result = "All links matched!"
-# if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches.
-#   There should be %i matches" %(len( matches ), len( answer_data ) )
-# else:
-#   for i in range( len(answer_data) ):
-#     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" %
-#       ( matches[i], answer_data[i] )
-#       break
-# print( result )
+result = "All links matched!"
+if len(matches) != len(answer_data):
+    result = "Your regex found %i matches. There should be %i matches" % (len(matches), len(answer_data))
+else:
+    for i in range(len(answer_data)):
+        if(matches[i] != answer_data[i]):
+            result = "Mismatched link. Got %s but expected %s" % (matches[i], answer_data[i])
+            break
+print(result)
