8b29faa958908d7456f761cc7b8712a391b7dcd8
robsalzberg <robsalzberg@gmail.com>
2019-08-01 23:08:11 -0400

    Save links in answers.txt to an answer_data list

--- legacy/projects/state-mach-regex/extractlinks/extract_links.py
+++ legacy/projects/state-mach-regex/extractlinks/extract_links.py
@@ -1,4 +1,6 @@
-import re   # module for processing regular expressions https://docs.python.org/3/library/re.html
+# module for processing regular expressions
+# https://docs.python.org/3/library/re.html
+import re
 import sys
 import csv
 if __name__ == '__main__':
@@ -19,7 +21,8 @@ file.close()
 
 # TODO Set up regex
 
-regex = 'https?:\/\/[^\"\ \<\>\{\}\|\\\^\~\[\]\']+?\.[^\"\ \<\>\{\}\|\\\^\~\[\]\']+'
+regex =
+'https?:\/\/[^\"\ \<\>\{\}\|\\\^\~\[\]\']+?\.[^\"\ \<\>\{\}\|\\\^\~\[\]\']+'
 
 # TODO Find links using regex, save in list called 'matches'
 
@@ -29,15 +32,21 @@ matches = re.findall(regex, filedata)
 # TODO Read in links from answers.txt (hint...this is a CSV file)
 # save in list called 'answer_data'
 
+with open("answers.txt") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',', quoting=csv.QUOTE_ALL)
+    for row in csv_reader:
+        answer_data = row
 
 # Compare answers with matches found using regex, print out any mismatches
 # UNCOMMENT BELOW WHEN READY TO CHECK IF YOUR REGEX IS FINDING ALL THE LINKS
 # result = "All links matched!"
 # if len( matches ) != len( answer_data ):
-#   result = "Your regex found %i matches. There should be %i matches" %(len( matches ), len( answer_data ) )
+#   result = "Your regex found %i matches.
+#   There should be %i matches" %(len( matches ), len( answer_data ) )
 # else:
 #   for i in range( len(answer_data) ):
 #     if( matches[i] != answer_data[i] ):
-#       result = "Mismatched link. Got %s but expected %s" % ( matches[i], answer_data[i] )
+#       result = "Mismatched link. Got %s but expected %s" %
+#       ( matches[i], answer_data[i] )
 #       break
 # print( result )
